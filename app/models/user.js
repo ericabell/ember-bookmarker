da084960fb96143d33428991f89776bf21bffee9
Eric Abell <ericabell@users.noreply.github.com>
2017-10-10 13:47:24 -0500

    Generated the user model along with the attributes and their types
    the ember cli is pretty awesome!

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  username: DS.attr('string'),
+  email: DS.attr('string'),
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  avatar: DS.attr('string'),
+  isAdmin: DS.attr('boolean'),
+  created: DS.attr('date')
 });

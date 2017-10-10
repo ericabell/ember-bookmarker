5fe61024fcd857ea498b16db435be5598f6fc846
Eric Abell <ericabell@users.noreply.github.com>
2017-10-10 11:13:06 -0500

    Add 'edit' nested route for bookmarks with id parameter

--- app/router.js
+++ app/router.js
@@ -10,6 +10,10 @@ Router.map(function() {
   this.route('about');
   this.route('bookmarks', function() {
     this.route('new');
+
+    this.route('edit', {
+      path: '/edit/:bookmark_id'
+    });
   });
 });
 

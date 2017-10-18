363a9529f343721f1865c3db2b4819f5a7a6380e
Eric Abell <ericabell@users.noreply.github.com>
2017-10-17 19:31:09 -0500

    Action hooks added for edit form
    Hooks for:
    1. save(bookmark)
    2. cancel()
    
    These are the action handlers that will be run when the user clicks
    on the buttons in our form.

--- app/components/bookmarks/edit-form.js
+++ app/components/bookmarks/edit-form.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save(bookmark) { Ember.logger.info('save called');},
+    cancel() { Ember.logger.info('cancel called');}
+  }
 });

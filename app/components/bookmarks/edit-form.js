1625a6f08be2d7fb5801fa68903e208db574e940
Eric Abell <ericabell@users.noreply.github.com>
2017-10-18 12:05:31 -0500

    Add save functionality in the action hook for edit-form
    Call the save() on the new bookmark, log the new value, and then
    redirect to the bookmarks index page.

--- app/components/bookmarks/edit-form.js
+++ app/components/bookmarks/edit-form.js
@@ -1,8 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  bookmark: null,
+  router: Ember.inject.service('-routing'),
+
   actions: {
-    save(bookmark) { Ember.Logger.info('save called');},
+    save(bookmark) { Ember.Logger.info('save called');
+    bookmark.save()
+      .then(
+        (value) => {
+          Ember.Logger.info(value);
+          this.get('router').transitionTo('bookmarks');
+        }
+      )
+  },
     cancel() { Ember.Logger.info('cancel called');}
   }
 });

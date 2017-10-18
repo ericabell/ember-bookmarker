444fbb32fc0ca263219db8e6da396288d7f95d52
Eric Abell <ericabell@users.noreply.github.com>
2017-10-18 14:32:48 -0500

    Verify that we can visit the edit bookmark page and click delete
    The test doesn't actually verify that the bookmark was deleted, only
    that after we clicked delete we were redirected back to the bookmark
    index page.

--- tests/acceptance/bookmark-delete-test.js
+++ tests/acceptance/bookmark-delete-test.js
@@ -4,9 +4,16 @@ import moduleForAcceptance from 'ember-bookmarker/tests/helpers/module-for-accep
 moduleForAcceptance('Acceptance | bookmark delete');
 
 test('visiting /bookmark-delete', function(assert) {
-  visit('/bookmark-delete');
+  let user = server.create('user', {});
+  let bookmark = server.create('bookmark', {
+    title: 'Test Bookmark',
+    userId: user.id
+  });
+  visit(`/bookmarks/edit/${bookmark.id}`);
+  click('button#delete');
 
   andThen(function() {
-    assert.equal(currentURL(), '/bookmark-delete');
-  });
+    assert.equal(currentURL(), '/bookmarks');
+  })
+
 });
